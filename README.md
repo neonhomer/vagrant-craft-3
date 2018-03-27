cbffc2b2ece38c708af09372a228afb2fa01fdda
Kramer <kramer@neonhomer.com>
2018-03-27 14:55:35 -0400

    Added Xdebug and Visual Studio Code launch.json Changed config editing to only save backup of original files Removed custom globals for siteUrl as @web alias replaces it

--- README.md
+++ README.md
@@ -1,21 +1,33 @@
 # Vagrant Craft 3
 
-Starter Craft CMS 3 setup using Vagrant for local development. Initial starter site url is http://localhost:8378. Depending on network environment the site will be accessible to other devices via [http://COMPUTER_NAME:8378](http://COMPUTER_NAME:8378).
+Starter Craft CMS 3 setup using Vagrant for local development. Initial starter site url is http://localhost:8378. Depending on network environment the site should be accessible to other devices via [http://COMPUTER_NAME:8378](http://COMPUTER_NAME:8378).
 
-Vagrant settings support macOS and Windows. Virtual machine is running Ubuntu 16 with PHP 7.0. Port number can be changed as desired in the Vagrantfile.
+Vagrant settings support macOS and Windows. Virtual machine is running Ubuntu 16 with PHP 7.0. Port number can be changed as desired in the Vagrantfile. 
+
+Installation includes Xdebug for PHP debugging and Visual Studio Code launch.json.
+
+## Requirements
+
+- [Vagrant](https://www.vagrantup.com/)
+- [Virtual Box](https://www.virtualbox.org/)
 
 ## Instructions
 
 1. Clone/download repository
-2. Change the main folder name to your project
-2. Run `vagrant up`
-3. Go to http://localhost:8378/index.php?p=admin to finalize Craft installation
-   1. Enter database information, Password=**password** and Datbase Name=**craft**
-   2. Create first admin account
-   3. Enter site details, *clear out the http://localhost reference and leave blank*
+1. Change the main folder name to your project name
+	1. If desired change port number in Vagrantfile
+1. Run `vagrant up`
+1. Go to http://localhost:8378/admin to finalize Craft installation
+   1. Enter database information, Password = **password** and Datbase Name = **craft**
+   1. Create first admin account
+   1. Enter site details, leaving Base URL = **@web**
 
 *\*Note: If cloned the .gitignore and .git files will need to be replaced.*
 
-## Requirements
-- [Vagrant](https://www.vagrantup.com/)
-- [Virtual Box](https://www.virtualbox.org/)
+## Changelog
+
+**2018-03-27**
+
+- Added Xdebug
+- Added Visual Studio Code launch.json for debugging with Xdebug
+- Removed custom globals URI_PORT and SITE_URL by using @web alias for siteUrl
--- bootstrap.sh
+++ bootstrap.sh
@@ -14,6 +14,7 @@ sudo apt-get install -y php imagemagick php-imagick php-curl php-zip
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $DATABASE_PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DATABASE_PASSWORD"
 sudo apt-get -y install mysql-server php-mysql
+sudo apt-get -y install php-xdebug
 
 # Install PHPMyAdmin
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
@@ -29,12 +30,19 @@ sudo mysql -u root -p$DATABASE_PASSWORD -e "ALTER DATABASE $DATABASE CHARACTER S
 sudo mysql -u root -p$DATABASE_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
 # Turn on PHP errors and increase upload file size limit
-sudo sed -i.bak "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/apache2/php.ini
-sudo sed -i.bak "s/display_errors = .*/display_errors = On/" /etc/php/7.0/apache2/php.ini
-sudo sed -i.bak "s/upload_max_filesize = .*/upload_max_filesize = 16M/" /etc/php/7.0/apache2/php.ini
+sudo cp /etc/php/7.0/apache2/php.ini /etc/php/7.0/apache2/php.ini.bak
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/apache2/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.0/apache2/php.ini
+sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 16M/" /etc/php/7.0/apache2/php.ini
+
+# Enable PHP Xdebug, log file is initially commented out
+sudo sed -i "$ a\ \n[Xdebug]\nxdebug.remote_enable = 1\nxdebug.remote_autostart = 1\nxdebug.remote_connect_back = 1\n; xdebug.remote_log = /vagrant/xdebug.log" /etc/php/7.0/apache2/php.ini
+echo "Edited php.ini"
 
 # AllowOverride in apache
-sudo sed -i.bak "s/\tAllowOverride None/\tAllowOverride All/g" /etc/apache2/apache2.conf 
+sudo cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.bak
+sudo sed -i "s/\tAllowOverride None/\tAllowOverride All/g" /etc/apache2/apache2.conf 
+echo "Edited apache2.conf"
 
 # Enable mod_rewrite/mcrypt and restart Apache
 sudo a2enmod rewrite
@@ -48,18 +56,25 @@ sudo mv composer.phar /usr/bin/composer
 # Install Craft CMS (if not already installed)
 if ! [ -f /vagrant/craft/web/index.php ]; then
   composer create-project -s RC craftcms/craft /vagrant/craft
+
+	# Craft adjustments
+	# These are all optional
+
+	# First back up general config
+	sudo cp /vagrant/craft/config/general.php /vagrant/craft/config/general.php.bak
+	# Use email as username
+	sudo sed -i "s/'securityKey' => getenv('SECURITY_KEY'),/'securityKey' => getenv('SECURITY_KEY'),\n        'useEmailAsUsername' => true,/" /vagrant/craft/config/general.php
+	# Add index file if none exist
+	if ! [ -f /vagrant/craft/templates/index.twig ]; then
+		sudo touch /vagrant/craft/templates/index.twig
+		sudo printf "<html>\n<head></head>\n<body>\n{{ dump(_context) }}\n</body>\n</html>\n" > /vagrant/craft/templates/index.twig
+	fi
+	echo "Customized Craft CMS"
 fi
 
 # Set apache to use craft "web" folder
 if ! [ -L /var/www/html ]; then
 	sudo rm -rf /var/www/html
 	sudo ln -fs /vagrant/craft/web /var/www/html
+	echo "Symlinked craft/web folder"
 fi
-
-# Craft setting adjustments
-
-# Adjust local dev to use port number and set siteUrl
-sudo sed -i.bak "s/return \[/\/\/ Define URL settings to use port numbers for local development\ndefine('URI_PORT', (\$_SERVER['SERVER_PORT'] == '80') ? '' : ':' . \$_SERVER['SERVER_PORT']);\ndefine('SITE_URL', 'http:\/\/' . \$_SERVER['SERVER_NAME'] . URI_PORT . '\/');\n\nreturn [/" /vagrant/craft/config/general.php
-sudo sed -i.bak "s/'siteUrl' => null,/'siteUrl' => SITE_URL,/" /vagrant/craft/config/general.php
-# Use email as username (optional)
-sudo sed -i.bak "s/'securityKey' => getenv('SECURITY_KEY'),/'securityKey' => getenv('SECURITY_KEY'),\n        'useEmailAsUsername' => true,/" /vagrant/craft/config/general.php
